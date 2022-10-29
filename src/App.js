c286afe38b38b4bf1ffe1dda7a063170d8716231
Emerson Santos <emersonsebastiao625@gmail.com>
2022-10-29 19:36:39 -0300

    brincada

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { PrimeiroConteudo } from "./components/First-content";
 import { Header } from "./components/Header";
+import { SegundoConteudo } from "./components/Second-content";
 
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
 
     <Header />
     <PrimeiroConteudo />
+    <SegundoConteudo />
+    
     </div>
   );
 }
--- src/components/Second-content/index.js
+++ src/components/Second-content/index.js
@@ -1,1 +1,90 @@
-// criação da estrutura do componente second-content
+// criação da estrutura do componente second-content 'O que ofereceomos?'
+import './style.css'
+import TioRubens from './imgs/TioRubens.jpeg'
+
+export function SegundoConteudo () {
+    return(
+
+        <>
+
+            <section>
+        
+                <div className='parteAzul'>
+
+                    <h1>O que oferecemos?</h1>
+                    <h2>Para os responsáveis:</h2>
+
+                </div>
+
+                <div id='container'>
+
+                    <div>
+                        <div className='funcionalidades'>
+                        função 1
+                        </div>
+
+                        <h1>Função</h1>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut s </p>
+
+                    </div>
+
+                    <div>
+                        <div className='funcionalidades'>
+                        função 2
+                        </div>
+
+                        <h1>Função</h1>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut s </p>
+
+                    </div>
+
+                    <div>
+                        <div className='funcionalidades'>
+                        função 3
+                        </div>
+
+                        <h1>Função</h1>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut s </p>
+
+                    </div>
+
+                    <div>
+                        <div className='funcionalidades'>
+                        função 4
+                        </div>
+
+                        <h1>Função</h1>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut s </p>
+                        
+                    </div>
+
+                </div>
+
+                <div className='paraCondutor'>
+
+                    <div id='tio'>
+
+                        <img src={TioRubens} alt=''/>
+
+                    </div>
+
+                        <span> 
+                        
+                        <h1>Para os Condutores:</h1>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut s </p>
+                        
+                        </span>
+
+                    </div>  
+        
+            </section>
+        </>
+
+    )
+}
+

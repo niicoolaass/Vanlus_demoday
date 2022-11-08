f9be3461095a07a75c138b111176cfe1d5bd078b
Emerson Santos <emersonsebastiao625@gmail.com>
2022-11-07 23:16:05 -0300

    arrumando navegação

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import {Routes,Route,Link} from 'react-router-dom'
+import {Routes,Route} from 'react-router-dom'
 import Home from './Paginas/Home/Home.js'
 import Login from './Paginas/page2/Login.js';
 
 function App () {
-    return(
 
+    return(
         <>
-            
             <Routes>
-                <Route path='/Home' element={<Home />}/>
+                <Route path='/' element={<Home />}/>
                 <Route path='/page2' element={<Login />}/>
             </Routes>
-
         </>
-
     )
 }
 

4317645bf0d1e34de7df2b2891c00c8818bec261
Gabriel Romao <gabrielrromao@outlook.com>
2022-11-14 23:59:09 -0300

    recriando rotas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Home from './Paginas/Home/Home.js';
 import Login from './Paginas/Login/Login.js';
 import DadosCondutor1 from './Paginas/CadastroCondutor/components/Cadastro1/DadosCondutor1.js';
 import { Cadastro } from './Paginas/Cadastro/Cadastro.js';
+import  DadosCondutor2  from "./Paginas/CadastroCondutor/components/Cadastro2/DadosCondutor2";
+import  DadosCondutor3  from "./Paginas/CadastroCondutor/components/Cadastro3/DadosCondutor3";
+import  DadosCondutor4  from "./Paginas/CadastroCondutor/components/Cadastro4/DadosCondutor4";
+import  DadosCondutor5  from "./Paginas/CadastroCondutor/components/Cadastro5/DadosCondutor5";
+import  DadosCondutor6  from "./Paginas/CadastroCondutor/components/Cadastro6/DadosCondutor6";
 
 function App () {
 
@@ -14,6 +19,11 @@ function App () {
                 <Route path='/Login' element={<Login />}/>
                 <Route path='/CadastroCondutor' element={<DadosCondutor1 />}/>
                 <Route path='/Cadastro' element={<Cadastro />}/>
+                <Route  path='/cadastro2' exact element={<DadosCondutor2 />}/>    
+                <Route  path='/cadastro3' exact element={<DadosCondutor3 />}/>                   
+                <Route  path='/cadastro4' exact element={<DadosCondutor4 />}/>                   
+                <Route  path='/cadastro5' exact element={<DadosCondutor5 />}/>                   
+                <Route  path='/cadastro6' exact element={<DadosCondutor6 />}/>    
             </Routes>
             
         </>

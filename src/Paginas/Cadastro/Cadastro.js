ea35e58241e7a32241b0e15dfa53380735737085
Emerson Santos <emersonsebastiao625@gmail.com>
2022-11-14 23:20:36 -0300

    backup

--- src/Paginas/Cadastro/Cadastro.js
+++ src/Paginas/Cadastro/Cadastro.js
@@ -1,6 +1,8 @@
 import './Cadastro.css'
 import Logo from '../../styles/imgs/Logo 3.png'
 import CadastroPais from './imgs/CadastroPais.png'
+import CadastroCondutor from './imgs/CadastroCondutor.png'
+import { Link } from 'react-router-dom'
 
 export function Cadastro () {
     return(
@@ -8,18 +10,27 @@ export function Cadastro () {
         
             <section id='containerCadastro'>
 
+                    <img id='logo' src={Logo} alt='Vanlus'/>
                 
                 <div id='left'>
+                    
+                    <div className='left'> 
 
-                    <img id='logo' src={Logo} alt='Vanlus'/>
-
-                    <img id='CadastroPais' src={CadastroPais} alt='Cadastro Pais' /> 
+                        <Link><img id='CadastroPais' src={CadastroPais} alt='Cadastro Pais' /> </Link>
+                    
+                    </div>
 
                 </div>
 
+
+
                 <div id='right'>
 
+                    <div className='right'> 
 
+                        <Link to='/CadastroCondutor'><img id='CadastroCondutor' src={CadastroCondutor} alt='Cadastro Condutor' />  </Link>
+
+                    </div>
 
                 </div>
 

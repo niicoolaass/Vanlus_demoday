5a4f8d2fac13f3892792b83442d2923098d34615
Cristhian de Souza <cristhian.souza.06.2003@gmail.com>
2022-11-28 16:09:26 -0300

    melhoria no codigo

--- src/Paginas/HomeResponsavel/HomeResponsavel.js
+++ src/Paginas/HomeResponsavel/HomeResponsavel.js
@@ -1,14 +1,24 @@
 import './HomeResponsavel.css';
-import Template from '../Template/Template';
 import Sidebar from '../Template/components/Sidebar/Sidebar';
 import Filter from './components/Filter/Filter';
 import  Boy from './img/boy.jpg'
+import ChatOverlay from '../Template/components/Chat_Overlay/ChatOverlay';
+import Header from '../Template/components/Header/Header';
 
 function HomeResponsavel() {
 
     return (
         <>
-        <Template sidebar={Sidebar().homeResponsavel()} cor="modal-user blue" element={<Filter/>}/>
+        <div className="homeR">
+        {Sidebar().homeResponsavel()}
+            <div>
+                <Header cor="modal-user blue" />
+                <div style={{ display: "flex" }}>
+                <Filter/>
+                </div>
+            </div>
+            <ChatOverlay/>
+        </div>
         <img alt="sidebar-img" src={Boy} height="45%" style={{position:"fixed", bottom:"0", left:"0", zIndex:"-1"}}/>
         </>
         
--- src/Paginas/SeuCondutor/SeuCondutor.js
+++ src/Paginas/SeuCondutor/SeuCondutor.js
@@ -1,19 +1,27 @@
 import './SeuCondutor.css';
-import Template from '../Template/Template';
 import Sidebar from '../Template/components/Sidebar/Sidebar';
 import  Boy from '../HomeResponsavel/img/boy.jpg';
 import ContaSeuCondutor from '../Template/components/Account/ContaSeuCondutor';
+import ChatOverlay from '../Template/components/Chat_Overlay/ChatOverlay';
+import Header from '../Template/components/Header/Header';
 
 
 function SeuCondutor() {
 
     return (
         <>
-        <Template sidebar={Sidebar().seuCondutor()} cor="modal-user blue" element={<ContaSeuCondutor/>}/>
+        <div className="homeR">
+        {Sidebar().seuCondutor()}
+            <div>
+                <Header cor="modal-user blue" />
+                <div style={{ display: "flex" }}>
+                <ContaSeuCondutor/>
+                </div>
+            </div>
+            <ChatOverlay/>
+        </div>
         <img alt="sidebar-img" src={Boy} height="45%" style={{position:"fixed", bottom:"0", left:"0", zIndex:"-1"}}/>
         </>
-        
-
     )
 }
 
